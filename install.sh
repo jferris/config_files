a6f8abb51b75fb8d44805b18133ddfaafc1f77f8
Joe Ferris <jferris@Shiny.local>
2008-08-29 19:15:16 -0400

    Added a script to install config files into the user's home directory

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+for name in *; do
+  target="$HOME/.$name"
+  if [ -e $target ]; then
+    if [ ! -L $target ]; then
+      echo "WARNING: $target exists but is not a symlink."
+    fi
+  else
+    if [[ $name != 'install.sh' ]]; then
+      echo "Creating $target"
+      ln -s "$PWD/$name" "$target"
+    fi
+  fi
+done
